47b723cd1aeb12eeb0c2918d3d9b3f56fd326f74
john-selwyn <johnselwynraner@gmail.com>
2026-09-11 11:46:28 +0800

    Use PostgreSQL database

--- vps_portal/settings.py
+++ vps_portal/settings.py
@@ -40,8 +40,12 @@ TEMPLATES = [{
 WSGI_APPLICATION = "vps_portal.wsgi.application"
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "vps_portal",
+        "USER": "vps_django",
+        "PASSWORD": "ict@1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 LANGUAGE_CODE = "en-us"
